86860dd38304d5a1b3e260b491ef8dffa221cf57
Aaron Schuyler <asky28@gmail.com>
2020-05-17 19:21:13 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,8 +1,14 @@
 class Anagram
-  attr_accessor :anagrams
+  attr_accessor :word
   def initialize(word)
-    
+    @word = word
   end
-  def match
+  def match(array)
+    our_word = @word.split.sort
+    array.each do |word|
+      if our_word == word.split.sort
+      return word
+    end
+    end 
   end
 end
